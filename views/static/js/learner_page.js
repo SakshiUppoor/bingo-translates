9b71eb4a89f7fe6e80ad3c36575db4ff73bfc7b9
SiddhiJhunjhunwala <siddhi2000jhun@gmail.com>
2020-12-19 23:18:44 +0530

    learners panels translation

--- views/static/js/learner_page.js
+++ views/static/js/learner_page.js
@@ -1,6 +1,41 @@
+const container = document.getElementsByClassName("container")[0];
+const original_words = [
+  "Thankyou",
+  "What is the cost?",
+  "Do you speak English?",
+  "Can you help me?",
+  "Sorry",
+  "Nice to meet you!",
+];
+
+original_words.forEach((word, i) => {
+  translation = fetch(
+    `http://localhost:3000/translated?source_lan=en&res_lan=hi&inputstring=${word}`
+  )
+    .then((res) => {
+      res.json().then((data) => {
+        console.log(data);
+        const result = data.translatedText;
+        // return result;
+        const panel = `<div class="panel p1"><div class="translation"><div class="translated">${result}<div class="speaker" id="speakme${
+          i + 1
+        }"><ion-icon name="volume-high"></ion-icon></div></div><div class="pronunciation">dhanyavaad</div></div><div class="origin">${word}</div></div>`;
+        container.innerHTML = container.innerHTML + panel;
+      });
+    })
+    .catch((e) => console.log(e));
+});
+
 const speakme1 = document.getElementById("speakme1");
 speakme1.addEventListener("click", () => {
-  readOutLoud("धन्यवाद ", "hi");
+  fetch(
+    `http://localhost:3000/translated?source_lan=en&res_lan=hi&inputstring=Thankyou`
+  ).then((res) => {
+    res.json().then((data) => {
+      const result = data.translatedText;
+      readOutLoud(result, "hi");
+    });
+  });
 });
 
 const speakme2 = document.getElementById("speakme2");
