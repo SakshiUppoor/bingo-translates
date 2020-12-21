20b028c2354fe4bb13595adbb53962b4758d608d
SakshiUppoor <sakshiuppoor@gmail.com>
2020-12-21 18:01:34 +0530

    Chat: Implement tranlate to native language feature

--- utils/messages.js
+++ utils/messages.js
@@ -1,5 +1,6 @@
 const generateMessage = (username , text)=>{
     return {
+        message_id: createUUID(),
         username : username,
         text : text,
         createdAt : new Date().getTime()
@@ -8,11 +9,25 @@ const generateMessage = (username , text)=>{
 
 const generateLocation= (username ,locationlink)=>{
     return {
+        message_id: createUUID(),
         username,
         locationlink ,
         createdAt :new Date().getTime()
     }
 
+}
+
+function createUUID(){
+   
+    let dt = new Date().getTime()
+    
+    const uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        const r = (dt + Math.random()*16)%16 | 0
+        dt = Math.floor(dt/16)
+        return (c=='x' ? r :(r&0x3|0x8)).toString(16)
+    })
+    
+    return uuid
 }
 
 module.exports ={
