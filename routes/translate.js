9aa197855a5fa356849150f3a2bac620c2eb6a50
Yash Chachad <chachadyash786@gmail.com>
2020-12-21 13:35:58 +0530

    Shifted languages and quiz data into json files

--- routes/translate.js
+++ routes/translate.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const translatte = require("translatte");
 const router = express();
+const fs = require("fs");
 
 router.get("/translated", (req, res) => {
   if (!req.query.inputstring) {
@@ -34,121 +35,8 @@ router.get("/getQuestions", (req, res) => {
       // {0 - English(en),1 - Hindi(hi), 2 - Marathi(mr),3- Gujrati(gu),4-Kannada(kn)
       // ,5-Malayalam(ml),6-German(de),7-Russian(ru),8-Spanish(es),9-Tamil(ta),10-Telugu(te)}
 
-      const words = [
-        [
-          "Hello",
-          "Thankyou",
-          "Greetings",
-          "Welcome",
-          "Good morning",
-          "How are you ?",
-          "Goodluck",
-        ],
-        [
-          "नमस्ते",
-          "धन्यवाद",
-          "अभिवादन",
-          "स्वागत हे",
-          "शुभ प्रभात",
-          "क्या हाल है ?",
-          "सौभाग्य",
-        ],
-        [
-          "नमस्कार",
-          "धन्यवाद",
-          "शुभेच्छा",
-          "स्वागत आहे",
-          "शुभ प्रभात",
-          "तू कसा आहेस ?",
-          "गुडब्लक",
-        ],
-        [
-          "નમસ્તે",
-          "આભાર",
-          "શુભેચ્છાઓ",
-          "સ્વાગત",
-          "સુપ્રભાત",
-          "તમે કેમ છો ?",
-          "સારા નસીબ",
-        ],
-        [
-          "ಹಲೋ",
-          "ಧನ್ಯವಾದಗಳು",
-          "ಶುಭಾಶಯಗಳು",
-          "ಸ್ವಾಗತ",
-          "ಶುಭೋದಯ",
-          "ನೀವು ಹೇಗಿದ್ದೀರಿ ?",
-          "ಒಳ್ಳೆಯದಾಗಲಿ",
-        ],
-        [
-          "ഹലോ",
-          "നന്ദി",
-          "ആശംസകൾ",
-          "സ്വാഗതം",
-          "സുപ്രഭാതം",
-          "എന്തൊക്കെയുണ്ട് ?",
-          "നല്ലതുവരട്ടെ",
-        ],
-        [
-          "Hallo",
-          "Vielen Dank",
-          "Schöne Grüße",
-          "Herzlich willkommen",
-          "Guten Morgen",
-          "Wie geht es dir ?",
-          "Viel Glück",
-        ],
-        [
-          "Здравствуйте",
-          "Спасибо",
-          "приветствия",
-          "Добро пожаловать",
-          "Доброе утро",
-          "Как поживаете ?",
-          "Удачи",
-        ],
-        [
-          "Hola",
-          "Gracias",
-          "Saludos",
-          "Bienvenidos",
-          "Buenos días",
-          "Cómo estás ?",
-          "Buena suerte",
-        ],
-        [
-          "வணக்கம்",
-          "நன்றி",
-          "வாழ்த்துக்கள்",
-          "வரவேற்பு",
-          "காலை வணக்கம்",
-          "நீங்கள் எப்படி இருக்கிறீர்கள் ?",
-          "Goodluck.",
-        ],
-        [
-          "మీరు ఎలా ఉన్నారు ?",
-          "స్వాగతం",
-          "గుడ్లక్",
-          "శుభాకాంక్షలు",
-          "హలో",
-          "శుభోదయం",
-          "ధన్యవాదాలు",
-        ],
-      ];
-
-      const languageMap = {
-        en: { lang: "English", code: 0 },
-        hi: { lang: "Hindi", code: 1 },
-        mr: { lang: "Marathi", code: 2 },
-        gu: { lang: "Gujrathi", code: 3 },
-        kn: { lang: "Kannada", code: 4 },
-        ml: { lang: "Malayalam", code: 5 },
-        de: { lang: "German", code: 6 },
-        ru: { lang: "Russian", code: 7 },
-        es: { lang: "Spanish", code: 8 },
-        ta: { lang: "Tamil", code: 9 },
-        te: { lang: "Telugu", code: 10 },
-      };
+      const words = require("./quizData.json");
+      const languageMap = require("./languages.json");
 
       const source = languageMap[req.query.source_lan];
       const response = languageMap[req.query.res_lan];
