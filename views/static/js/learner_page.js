a4ac1fab80e5c0810faa1f2e73b039ca0798954e
SiddhiJhunjhunwala <siddhi2000jhun@gmail.com>
2020-12-21 21:59:21 +0530

    complete learning

--- views/static/js/learner_page.js
+++ views/static/js/learner_page.js
@@ -1,24 +1,29 @@
-const container = document.getElementsByClassName("container")[0];
-const content = data[0];
-const src = $("#my-lang").val();
-const dest = $("#learn-lang").val();
+function learner() {
+  const container = document.getElementsByClassName("container")[0];
+  const content = data[0];
+  const src = $("#my-lang").val();
+  const dest = $("#learn-lang").val();
 
-const words = content[src];
-const translated_words = content[dest];
+  const words = content[src];
+  const translated_words = content[dest];
 
-for (var i = 0; i < 7; i++) {
-  const word = words[i];
-  const translated_word = translated_words[i];
+  container.innerHTML = "";
 
-  const panel = `<div class="panel p1"><div class="translation"><div class="translated">${translated_word}<div class="speaker speaker-button" onclick='readOutLoud(
+  for (var i = 0; i < 7; i++) {
+    const word = words[i];
+    const translated_word = translated_words[i];
+
+    const panel = `<div class="panel p1"><div class="translation"><div class="translated">${translated_word}<div class="speaker speaker-button" onclick='readOutLoud(
          "${translated_word}",
           "${dest}"
         )' ><ion-icon  id="speakme${
           i + 1
         }" name="volume-high"></ion-icon></div></div></div><div class="origin">${word}</div></div>`;
-  container.innerHTML = container.innerHTML + panel;
+    container.innerHTML = container.innerHTML + panel;
+  }
 }
 
+learner();
 // const speakme6 = document.getElementById("speakme6");
 // speakme6.addEventListener("click", () => {
 //   readOutLoud("आपसे मिलकर अच्छा लगा!", "hi");
