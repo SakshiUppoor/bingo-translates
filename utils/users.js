5db599712299fe49590796980893823e5f16a84f
SakshiUppoor <sakshiuppoor@gmail.com>
2021-01-13 11:56:11 +0530

    Fix no username or room name bug

--- utils/users.js
+++ utils/users.js
@@ -1,32 +1,40 @@
 const users =[]
 
 const addUser = ({id , username , room})=>{
-    //Clean the data
-    username = username.trim().toLowerCase()
-    room = room.trim().toLowerCase()
+    
+    if(username && room) {
 
-    //Validate the data
-    if(!username || !room){
-        return {
-            error : "Username and room are required !"
+        //Clean the data
+        username = username.trim().toLowerCase()
+        room = room.trim().toLowerCase()
+
+        //Validate the data
+        if(!username || !room){
+            return {
+                error : "Username and room are required !"
+            }
         }
-    }
 
-    //Check for existing users
-    const existingUser = users.find((user)=>{
-        return user.room === room && user.username === username
-    })
+        //Check for existing users
+        const existingUser = users.find((user)=>{
+            return user.room === room && user.username === username
+        })
 
-    if(existingUser){
-        return {
-            error : "Username is aldready in use !"
+        if(existingUser){
+            return {
+                error : "Username is aldready in use !"
+            }
         }
-    }
 
-    //Storing valid users
-    const user = {id , username , room }
-    users.push(user)
-    return { user }
+        //Storing valid users
+        const user = {id , username , room }
+        users.push(user)
+        return { user }
+    } else{
+        return {
+                error : "Username and room name are required!"
+            }
+    }
 }
 
 
